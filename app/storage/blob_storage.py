e4412124e8b5aac25a4d24e0321f87ed2ff06a69
Igor Martins <igormartins0301@gmail.com>
2024-10-09 19:02:49 -0300

    Refactored connection and file upload class

--- app/storage/blob_storage.py
+++ app/storage/blob_storage.py
@@ -11,35 +11,31 @@ load_dotenv()
 
 class BlobStorage:
     def __init__(self):
-        self.connection_string = self._get_secret_from_key_vault()
+        self.credentials = self._get_credentials()
+        self.account_url = os.environ.get('AZURE_STORAGE_URL')
         self.connection = self.connect()
 
     @staticmethod
-    def _get_secret_from_key_vault():
+    def _get_credentials():
         """Recupera o segredo do Azure Key Vault."""
         client_id = os.environ.get('AZURE_CLIENT_ID')
         tenant_id = os.environ.get('AZURE_TENANT_ID')
         client_secret = os.environ.get('AZURE_CLIENT_SECRET')
-        vault_url = os.environ.get('AZURE_VAULT_URL')
-        secret_name = os.environ.get('AZURE_SECRET_NAME')
+        
 
         credentials = ClientSecretCredential(
             client_id=client_id,
             client_secret=client_secret,
             tenant_id=tenant_id,
         )
-
-        secret_client = SecretClient(
-            vault_url=vault_url, credential=credentials
-        )
-        secret = secret_client.get_secret(secret_name)
-        return secret.value
+        return credentials
 
     def connect(self):
-        """Estabelece a conexão com o Blob Storage usando a connection string."""
+        """Estabelece a conexão com o Blob Storage usando as credenciais."""
         try:
-            blob_service_client = BlobServiceClient.from_connection_string(
-                self.connection_string
+            blob_service_client = BlobServiceClient(
+                account_url=self.account_url,
+                credential=self.credentials
             )
             print('Conexão estabelecida com sucesso!')
             return blob_service_client
@@ -47,14 +43,18 @@ class BlobStorage:
             print(f'Erro ao conectar ao Blob Storage: {str(e)}')
             return None
 
-    def upload(self, file_path, container_name, blob_name):
+    def upload(self, filename, file_path, container_name):
         """Realiza o upload de um arquivo para o Blob Storage."""
         try:
-            blob_client = self.connection.get_blob_client(
-                container=container_name, blob=blob_name
-            )
+            container_client = self.connection.get_container_client(container=container_name)
             with open(file_path, 'rb') as data:
-                blob_client.upload_blob(data, overwrite=True)
+                container_client.upload_blob(name=filename, data=data, overwrite=True)
             print('Arquivo enviado com sucesso!')
         except Exception as e:
             print(f'Erro ao fazer upload do arquivo: {str(e)}')
+
+
+con = BlobStorage()
+con.upload(filename='arquivo_teste.txt', 
+           file_path=r'/home/igor/projetos/data_validation/teste.txt', 
+           container_name='csv')
